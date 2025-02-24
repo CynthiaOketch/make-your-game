6c779512b82884a3fa392db96f28d374bb9934d4
coketch <cynthiaoketch6@gmail.com>
2025-02-24 12:40:14 +0300

    refactor code

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 let lastTime = 0;
+
 function gameLoop(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
-    // Call game update functions here
+    // Clear and update the game state
+    updateGame();
     handleCollisions();
 
     requestAnimationFrame(gameLoop);
 }
+
+function updateGame() {
+    console.log("Game is running!"); // Debugging: Check if game loop is running
+}
+
+// Start the game loop
 requestAnimationFrame(gameLoop);
